6a3f954385b5daaa9069c2142917c81f97d196bd
Nam Dang <nathandang73@gmail.com>
2025-04-15 01:15:07 +1000

    Implement startup card details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,26 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
-}: {
+}: Readonly<{
   searchParams: Promise<{ query?: string }>;
-}) {
+}>) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createAt: new Date(),
+      views: 55,
+      author: { id: 1, name: "Nathan" },
+      _id: 1,
+      description: "This is the startup description",
+      image: "https://picsum.photos/id/684/700/400",
+      category: "AI",
+      title: "The AI Startup",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -19,6 +34,21 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
